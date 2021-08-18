d3981bc368b0af6fb32a36fcbaa5b161f44d624d
chenjunyao <chen13157379235@outlook.com>
2021-08-18 16:44:11 +0800

    Update 34

--- Solutions/Algorithms/Python/Difficulty/Medium/34.在排序数组中查找元素的第一个和最后一个位置.py
+++ Solutions/Algorithms/Python/Difficulty/Medium/34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -7,7 +7,10 @@
 # @lc code=start
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [self.findLeftBound(nums, target), self.findRightBound(nums, target)]
+        left = self.findLeftBound(nums, target)
+        if left > len(nums) - 1 or nums[left] != target:
+            return [-1, -1]
+        return [left, self.findLeftBound(nums, target + 1) - 1]
 
     def findLeftBound(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
@@ -15,26 +18,40 @@ class Solution:
             mid = left + (right - left) // 2
             if nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
             else:
                 right = mid - 1
-        if left > len(nums) - 1 or nums[left] != target:
-            return - 1
         return left
-
-    def findRightBound(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        if right < 0 or nums[right] != target:
-            return - 1
-        return right
 # @lc code=end
+
+# class Solution:
+#     def searchRange(self, nums: List[int], target: int) -> List[int]:
+#         return [self.findLeftBound(nums, target), self.findRightBound(nums, target)]
+
+#     def findLeftBound(self, nums: List[int], target: int) -> int:
+#         left, right = 0, len(nums) - 1
+#         while left <= right:
+#             mid = left + (right - left) // 2
+#             if nums[mid] < target:
+#                 left = mid + 1
+#             elif nums[mid] > target:
+#                 right = mid - 1
+#             else:
+#                 right = mid - 1
+#         if left > len(nums) - 1 or nums[left] != target:
+#             return - 1
+#         return left
+
+#     def findRightBound(self, nums: List[int], target: int) -> int:
+#         left, right = 0, len(nums) - 1
+#         while left <= right:
+#             mid = left + (right - left) // 2
+#             if nums[mid] < target:
+#                 left = mid + 1
+#             elif nums[mid] > target:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+#         if right < 0 or nums[right] != target:
+#             return - 1
+#         return right
 
