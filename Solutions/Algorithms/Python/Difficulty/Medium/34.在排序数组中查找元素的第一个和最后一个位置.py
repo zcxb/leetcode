2e9b46834a94eac40ab493d09c1dade2e5eee3d7
chenjunyao <chen13157379235@outlook.com>
2021-08-18 16:31:18 +0800

    Add 34

---
+++ Solutions/Algorithms/Python/Difficulty/Medium/34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=34 lang=python3
+#
+# [34] 在排序数组中查找元素的第一个和最后一个位置
+#
+
+# @lc code=start
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return [self.findLeftBound(nums, target), self.findRightBound(nums, target)]
+
+    def findLeftBound(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                right = mid - 1
+        if left > len(nums) - 1 or nums[left] != target:
+            return - 1
+        return left
+
+    def findRightBound(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if right < 0 or nums[right] != target:
+            return - 1
+        return right
+# @lc code=end
+
